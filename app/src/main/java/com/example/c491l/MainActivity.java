8a1862d8bafda75ff432340a4f16093ed1688d20
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2020-04-02 23:58:29 -0700

    Converted adobe xd login to android

--- app/src/main/java/com/example/c491l/MainActivity.java
+++ app/src/main/java/com/example/c491l/MainActivity.java
@@ -4,15 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mLogin;
+    private ImageButton mLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
         mLogin = findViewById(R.id.buttonLogin);
         mLogin.setOnClickListener(new View.OnClickListener(){
             @Override
