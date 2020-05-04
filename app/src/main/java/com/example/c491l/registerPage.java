69d61aa41a727f3f34e1c0237154dea92e5dc7f8
herreraosmar83 <osmar.herreraperez.945@my.csun.edu>
2020-05-04 01:49:32 -0700

    made registration page

---
+++ app/src/main/java/com/example/c491l/registerPage.java
@@ -0,0 +1,41 @@
+package com.example.c491l;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class registerPage extends AppCompatActivity {
+    private ImageButton Login;
+    private EditText email;
+    private EditText user;
+    private EditText pass;
+    private EditText re_pass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_page);
+        Login = findViewById(R.id.buttonLogin2);
+
+        email = findViewById(R.id.emailR);
+        user = findViewById(R.id.UserR);
+        pass = findViewById(R.id.PassR);
+        re_pass = findViewById(R.id.reEnter);
+
+//        handle validation and registration here
+
+        Login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openCameraView();
+            }
+        });
+    }
+    public void openCameraView() {
+        Intent intent = new Intent(this, splash.class);
+        startActivity(intent);
+    }
+}
